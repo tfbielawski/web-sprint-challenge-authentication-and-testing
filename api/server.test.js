dafd3d5c15df73540b9d3a32d0266c24d8512f32
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-17 13:19:42 -0400

    testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 
 const server = require("./server")
-// const request = require('supertest');
-// const db = require('../data/dbConfig');
+const request = require('supertest');
+const db = require('../data/dbConfig');
 
 // Write your tests here
 test('sanity', () => {
@@ -15,51 +15,51 @@ describe("Server Auth Testing", () => {
     expect(server).toBeDefined();
   })
 
-  // beforeAll(async () =>   {
-  //   await db.migrate.rollback();
-  //   await db.migrate.latest();
-  // })
-  // Test register
-  // test("creates a user", async () => {
-  //   //Request from server, assign to res
-  //   const res = await request(server)
-  //       //Post user data to register endpoint
-  //       .post("/auth/register")
-  //       .send({username: "Captain Marvel", password: "foobar"})
-  //   expect(res).toBeTruthy();
-  //
-  // })
-  //
-  // test("Tests server does respond when incorrect endpoint reached", async ()=> {
-  //   const res = await request(server).get("/weirdo");
-  //   const answer = res.body;
-  //   expect(answer).toBeTruthy();
-  // })
+  beforeAll(async () =>   {
+    await db.migrate.rollback();
+    await db.migrate.latest();
+  })
+  //Test register
+  test("creates a user", async () => {
+    //Request from server, assign to res
+    const res = await request(server)
+        //Post user data to register endpoint
+        .post("/auth/register")
+        .send({username: "Captain Marvel", password: "foobar"})
+    expect(res).toBeTruthy();
 
-  // test("returns a message when accessing incorrect endpoint", async ()=> {
-  //   const res = await request(server).get("/weirdo");
-  //   const answer = res.body;
-  //   expect(answer).toMatchObject({ message: "HA!, The joke's on you!"});
-  // })
+  })
+
+  test("Tests server does respond when incorrect endpoint reached", async ()=> {
+    const res = await request(server).get("/weirdo");
+    const answer = res.body;
+    expect(answer).toBeTruthy();
+  })
+
+  test("returns a message when accessing incorrect endpoint", async ()=> {
+    const res = await request(server).get("/weirdo");
+    const answer = res.body;
+    expect(answer).toMatchObject({ message: "HA!, The joke's on you!"});
+  })
 
 
-  // test(" Jokes should be defined", function() {
-  //   return request(server)
-  //       .get("/api/jokes/")
-  //       .then(res => { expect(res.body).toBeDefined(); })
-  // })
-  //
-  //
-  // test("Jokes should return truthy", function() {
-  //   return request(server)
-  //       .get("/api/jokes/")
-  //       .then(res => { expect(res.type).toBeTruthy()})
-  // })
-  //
-  // it("Truth test", function() {
-  //   return request(server)
-  //       .post('/api/auth/register')
-  //       .send({ username: "test", password: "1234" })
-  //       .then(res => {expect(res.type).toBeTruthy(); })
-  // })
+  test(" Jokes should be defined", function() {
+    return request(server)
+        .get("/api/jokes/")
+        .then(res => { expect(res.body).toBeDefined(); })
+  })
+
+
+  test("Jokes should return truthy", function() {
+    return request(server)
+        .get("/api/jokes/")
+        .then(res => { expect(res.type).toBeTruthy()})
+  })
+
+  it("Truth test", function() {
+    return request(server)
+        .post('/api/auth/register')
+        .send({ username: "test", password: "1234" })
+        .then(res => {expect(res.type).toBeTruthy(); })
+  })
 })
